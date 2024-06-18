f4e38f2eb9e036d36922c08b55eb427c5c8f8e43
luke-weller <wellerluke@hotmail.com>
2024-06-18 18:08:30 -0400

    set up basic lambda and api gateway with dynamo db using cdk

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,16 +1,100 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CfnOutput, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import {
+  ApiKey,
+  ApiKeySourceType,
+  Cors,
+  LambdaIntegration,
+  RestApi,
+  UsagePlan,
+} from "aws-cdk-lib/aws-apigateway";
+import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
 
-export class InfrastructureStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class InfrastructureStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // create the db table
+    const dbTable = new Table(this, "DbTable", {
+      partitionKey: { name: "pk", type: AttributeType.STRING },
+      removalPolicy: RemovalPolicy.DESTROY,
+      billingMode: BillingMode.PAY_PER_REQUEST,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // create the api gateway
+    const api = new RestApi(this, "RestAPI", {
+      restApiName: "RestAPI",
+      defaultCorsPreflightOptions: {
+        allowOrigins: Cors.ALL_ORIGINS,
+        allowMethods: Cors.ALL_METHODS,
+      },
+      apiKeySourceType: ApiKeySourceType.HEADER,
+    });
+
+    // create the api key
+    const apiKey = new ApiKey(this, "ApiKey");
+
+    // create the usage plan
+    const usagePlan = new UsagePlan(this, "UsagePlan", {
+      name: "Usage Plan",
+      apiStages: [
+        {
+          api,
+          stage: api.deploymentStage,
+        },
+      ],
+    });
+
+    // add the api key to the usage plan
+    usagePlan.addApiKey(apiKey);
+
+    // create the lambda functions
+    const postsLambda = new NodejsFunction(this, "PostsLambda", {
+      entry: "resources/endpoints/posts.ts",
+      handler: "handler",
+      environment: {
+        TABLE_NAME: dbTable.tableName,
+      },
+    });
+
+    const postLambda = new NodejsFunction(this, "PostLambda", {
+      entry: "resources/endpoints/post.ts",
+      handler: "handler",
+      environment: {
+        TABLE_NAME: dbTable.tableName,
+      },
+    });
+
+    // Grant read and write access to the `postsLambda` and `postLambda` functions to the `dbTable`
+    dbTable.grantReadWriteData(postsLambda);
+    dbTable.grantReadWriteData(postLambda);
+
+    // define the endpoints
+    const posts = api.root.addResource("posts");
+    const post = posts.addResource("{id}");
+
+    const postsIntegration = new LambdaIntegration(postsLambda);
+    const postIntegration = new LambdaIntegration(postLambda);
+
+    // define the methods on the defined endpoints
+    posts.addMethod("GET", postsIntegration, {
+      apiKeyRequired: true,
+    });
+    posts.addMethod("POST", postsIntegration, {
+      apiKeyRequired: true,
+    });
+
+    post.addMethod("GET", postIntegration, {
+      apiKeyRequired: true,
+    });
+    post.addMethod("DELETE", postIntegration, {
+      apiKeyRequired: true,
+    });
+
+    // output the api key id
+    new CfnOutput(this, "API Key ID", {
+      value: apiKey.keyId,
+    });
   }
 }
